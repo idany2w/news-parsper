bb7b42566ba44d85be5e79ec01da0a6427290091
Daniil Ruzin <jeffgoldtv7@gmail.com>
2022-07-14 09:45:12 +0300

    start vueing

---
+++ resources/js/Pages/Article.vue
@@ -0,0 +1,25 @@
+<script >
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+export default {
+  props: {
+    id: Number,
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {}
+}
+</script>
+
+<template>
+    <Head title="Article" />
+
+    <!-- <Link :href="route('article')" >
+        Log in
+    </Link> -->
+
+    Article {{ id }}
+</template>
